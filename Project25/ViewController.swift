62a84d49365bbb60ed618550c489907be9323436
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-10-11 20:24:40 +0300

    Initial Commit

---
+++ Project25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project25
+//
+//  Created by othman shahrouri on 10/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
